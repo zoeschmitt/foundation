8de49e979754bd2fa38d5372c2edbf8ee12b3a2b
Zoe Schmitt <zoeschmitt@hotmail.com>
2022-03-29 20:06:35 -0500

    feat: upgraded smart contract to include royalties

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -3,33 +3,61 @@
 pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Royalty.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract NFT is ERC721 {
+contract NFT is ERC721URIStorage, ERC721Royalty {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
     string _tokenURI;
 
     constructor(string memory _name, string memory _symbol)
-        public
         ERC721(_name, _symbol)
     {}
 
-    function mintNFT(address recipient, string memory tokenURI)
+    function mintNFT(address recipient, string memory uri, uint96 royalty)
         public
         returns (uint256)
     {
-        _tokenURI = tokenURI;
+        _tokenURI = uri;
         _tokenIds.increment();
 
         uint256 newItemId = _tokenIds.current();
         _safeMint(recipient, newItemId);
-        _setTokenURI(newItemId, tokenURI);
+        _setTokenURI(newItemId, uri);
+        _setDefaultRoyalty(recipient, royalty);
 
         return newItemId;
     }
 
     function contractURI() public view returns (string memory) {
         return _tokenURI;
+    }
+
+    function _burn(uint256 tokenId)
+        internal
+        override(ERC721Royalty, ERC721URIStorage)
+    {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        virtual
+        override(ERC721, ERC721Royalty)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
     }
 }
